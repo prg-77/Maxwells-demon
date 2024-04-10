848f2f1bd681b2c3c68659bf2a253aa578b9530a
prg-77 <pabloramgav@gmail.com>
2024-04-10 21:37:45 +0200

    Actualización codigo.cpp

--- codigo.cpp
+++ codigo.cpp
@@ -1,59 +1,103 @@
 #include <iostream>
+#include <numeric>
 #include <string>
 #include <cmath>
 #include <fstream>
 #include <stdlib.h>
 #include <time.h>
 
-#define N1 3 //Número de celdas horizontales
-#define N2 3 //Número de celdas verticales
-#define num N1*N2/3 //Número de partículas   (La división es entera)
-#define num_pasos 100 //Número de pasos
-
 using namespace std;
 
-void inicializar_posiciones(int posiciones[][2], ofstream& datos_posiciones);
-void actualizar_posiciones(int posiciones[][2], ofstream& datos_posiciones);
-bool rodeado(int posiciones[][2], int i);
+void inicializar_posiciones(int posiciones[][2], int N[2], int num[], int tipos, ofstream* datos_posiciones);
+void actualizar_posiciones(int posiciones[][2], int N[2], int num[], int v[], int tipos, ofstream* datos_posiciones);
+bool rodeado(int posiciones[][2], int N[2], int num[], int i, int v, int tipos);
+int suma_array(int array[], int n);
 
 int main()
 {
-    int posiciones[num][2];
-    ofstream parametros_iniciales, datos_posiciones;
+    int N[2], tipos, num_pasos;
+
+    N[0] = 4; // Número de celdas horizontales
+    N[1] = 4; // Número de celdas verticales
+    tipos = 2; // Número de tipos de partículas
+    num_pasos = 100; // Número de pasos
+
+    int num[tipos], v[tipos];
+
+    // Si queremos el mismo número de partículas de cada tipo y velocidades ascendentes
+    for (int i = 0; i < tipos; i++)
+    {
+        num[i] = 1; // Número de partículas de cada tipo [num1, num2, ...]
+        v[i] = i+1; // Velocidad de las partículas de cada tipo [v1, v2, ...]
+    }
+    // Si queremos un número de partículas y velocidades diferentes para cada tipo
+    // num[0] = 1;
+    // num[1] = 1;
+    // v[0] = 1;
+    // v[1] = 2;
+
+    int posiciones[suma_array(num,tipos)][2];
+    ofstream parametros_iniciales, datos_posiciones[tipos];
 
     parametros_iniciales.open("parametros_iniciales.dat");
-    parametros_iniciales << "#N1 N2 num num_pasos" << endl;
-    parametros_iniciales << N1 << " , " << N2 << " , " << num << " , " << num_pasos << endl;
+
+    parametros_iniciales << "#N1 N2 num_pasos";
+
+    for (int i = 0; i < tipos; i++)
+    {
+        parametros_iniciales << " num" << i+1 << " v" << i+1;
+    }
+
+    parametros_iniciales << endl;
+
+    parametros_iniciales << N[0] << " , " << N[1] << " , " << num_pasos;
+
+    for (int i = 0; i < tipos; i++)
+    {
+        parametros_iniciales << " , " << num[i] << " , " << v[i];
+    }
+
     parametros_iniciales.close();
 
-    datos_posiciones.open("posiciones.dat");
-    inicializar_posiciones(posiciones, datos_posiciones);
+    for (int i = 0; i < tipos; i++)
+    {
+        datos_posiciones[i].open("posiciones" + to_string(i+1) + ".dat");
+    }
+
+    inicializar_posiciones(posiciones, N, num, tipos, datos_posiciones);
 
     for (int i = 0; i < num_pasos; i++)
     {
-        datos_posiciones << endl;
-        actualizar_posiciones(posiciones, datos_posiciones);
+        for (int j = 0; j < tipos; j++)
+        {
+            datos_posiciones[j] << endl;
+        }
+        actualizar_posiciones(posiciones, N, num, v, tipos, datos_posiciones);
     }
 
-    datos_posiciones.close();
+    for (int i = 0; i < tipos; i++)
+    {
+        datos_posiciones[i].close();
+    }
     return 0;
 }
 
-void inicializar_posiciones(int posiciones[][2], ofstream& datos_posiciones)
+void inicializar_posiciones(int posiciones[][2], int N[2], int num[], int tipos, ofstream* datos_posiciones)
 {
     bool diferente;
+    int contador = 0;
 
     srand(time(NULL));
 
-    for (int i = 0; i < num; i++)
+    for (int i = 0; i < (suma_array(num,tipos)); i++)
     {
         diferente = false;
         while (diferente == false)
         {
             diferente = true;
 
-            posiciones[i][0] = floor(1.0*N1*rand()/RAND_MAX);
-            posiciones[i][1] = floor(1.0*N2*rand()/RAND_MAX);
+            posiciones[i][0] = floor(1.0*N[0]*rand()/RAND_MAX);
+            posiciones[i][1] = floor(1.0*N[1]*rand()/RAND_MAX);
 
             for (int j = 0; j < i; j++)
             {
@@ -64,90 +108,101 @@ void inicializar_posiciones(int posiciones[][2], ofstream& datos_posiciones)
                 }
             }
         }
-        datos_posiciones << posiciones[i][0]+1 << " , " << posiciones[i][1]+1 << endl;
     } 
+    for (int j = 0; j < tipos; j++)
+    {
+        for(int i=contador; i < suma_array(num,j+1); i++)
+        {
+            datos_posiciones[j] << posiciones[i][0]+1 << " , " << posiciones[i][1]+1 << endl;
+        }
+        contador = suma_array(num,j+1);
+    }
 }
 
-void actualizar_posiciones(int posiciones[][2], ofstream& datos_posiciones)
+void actualizar_posiciones(int posiciones[][2], int N[2], int num[], int v[], int tipos, ofstream* datos_posiciones)
 {
-    double r;
+    double r, posiciones_aux[2];
     bool ocupado;
-    double posiciones_aux[2];
+    int contador = 0;
 
-    for (int i = 0; i < num; i++)
+    for (int j=0; j<tipos; j++)
     {
-        if (rodeado(posiciones, i))
+        for (int i = contador; i < suma_array(num,j+1); i++)
         {
-            datos_posiciones << posiciones[i][0]+1 << " , " << posiciones[i][1]+1 << endl;
-            continue;
-        }
-        
-        ocupado = true;
-
-        while (ocupado == true)
-        {
-            ocupado = false;
-
-            r = 1.0*rand()/RAND_MAX;
-            //cout << r << endl;
-
-            if (r < 0.25)
+            if (rodeado(posiciones, N, num, i, v[j], tipos))
             {
-                posiciones_aux[0] = posiciones[i][0] + 1;
-                posiciones_aux[1] = posiciones[i][1];
-            }
-            else if (r < 0.5)
-            {
-                posiciones_aux[0] = posiciones[i][0] - 1;
-                posiciones_aux[1] = posiciones[i][1];
-            }
-            else if (r < 0.75)
-            {
-                posiciones_aux[0] = posiciones[i][0];
-                posiciones_aux[1] = posiciones[i][1] + 1;
-            }
-            else
-            {
-                posiciones_aux[0] = posiciones[i][0];
-                posiciones_aux[1] = posiciones[i][1] - 1;
-            }
-
-            if (posiciones_aux[0] < 0 || posiciones_aux[0] >= N1 || posiciones_aux[1] < 0 || posiciones_aux[1] >= N2)
-            {
-                ocupado = true;
+                datos_posiciones[j] << posiciones[i][0]+1 << " , " << posiciones[i][1]+1 << endl;
                 continue;
             }
+            
+            ocupado = true;
 
-            for (int j = 0; j < num; j++)
+            while (ocupado == true)
             {
-                if (posiciones_aux[0] == posiciones[j][0] && posiciones_aux[1] == posiciones[j][1])
+                ocupado = false;
+
+                r = 1.0*rand()/RAND_MAX;
+                //cout << r << endl;
+
+                if (r < 0.25)
+                {
+                    posiciones_aux[0] = posiciones[i][0] + v[j];
+                    posiciones_aux[1] = posiciones[i][1];
+                }
+                else if (r < 0.5)
+                {
+                    posiciones_aux[0] = posiciones[i][0] - v[j];
+                    posiciones_aux[1] = posiciones[i][1];
+                }
+                else if (r < 0.75)
+                {
+                    posiciones_aux[0] = posiciones[i][0];
+                    posiciones_aux[1] = posiciones[i][1] + v[j];
+                }
+                else
+                {
+                    posiciones_aux[0] = posiciones[i][0];
+                    posiciones_aux[1] = posiciones[i][1] - v[j];
+                }
+
+                if (posiciones_aux[0] < 0 || posiciones_aux[0] >= N[0] || posiciones_aux[1] < 0 || posiciones_aux[1] >= N[1])
                 {
                     ocupado = true;
                     continue;
                 }
-            }  
+
+                for (int k = 0; k < suma_array(num,tipos); k++)
+                {
+                    if (posiciones_aux[0] == posiciones[k][0] && posiciones_aux[1] == posiciones[k][1])
+                    {
+                        ocupado = true;
+                        continue;
+                    }
+                }  
+            }
+            posiciones[i][0] = posiciones_aux[0];
+            posiciones[i][1] = posiciones_aux[1];
+            datos_posiciones[j] << posiciones[i][0]+1 << " , " << posiciones[i][1]+1 << endl;
         }
-        posiciones[i][0] = posiciones_aux[0];
-        posiciones[i][1] = posiciones_aux[1];
-        datos_posiciones << posiciones[i][0]+1 << " , " << posiciones[i][1]+1 << endl;
+        contador = suma_array(num,j+1);
     }
 }
 
-bool rodeado(int posiciones[][2], int i)
+bool rodeado(int posiciones[][2], int N[2], int num[], int i, int v, int tipos)
 {
     bool rodeado[4] = {false, false, false, false};
-    int pos_proximas[4][2] = {{posiciones[i][0]+1, posiciones[i][1]}, {posiciones[i][0]-1, posiciones[i][1]}, {posiciones[i][0], posiciones[i][1]+1}, {posiciones[i][0], posiciones[i][1]-1}};
+    int pos_proximas[4][2] = {{posiciones[i][0]+v, posiciones[i][1]}, {posiciones[i][0]-v, posiciones[i][1]}, {posiciones[i][0], posiciones[i][1]+v}, {posiciones[i][0], posiciones[i][1]-v}};
 
     for (int j = 0; j < 4; j++)
     {
-        if (pos_proximas[j][0] < 0 || pos_proximas[j][0] >= N1 || pos_proximas[j][1] < 0 || pos_proximas[j][1] >= N2)
+        if (pos_proximas[j][0] < 0 || pos_proximas[j][0] >= N[0] || pos_proximas[j][1] < 0 || pos_proximas[j][1] >= N[1])
         {
             rodeado[j] = true;
             continue;
         }
         else
         {
-            for (int k = 0; k < num; k++)
+            for (int k = 0; k < suma_array(num,tipos); k++)
             {
                 if (pos_proximas[j][0] == posiciones[k][0] && pos_proximas[j][1] == posiciones[k][1])
                 {
@@ -166,4 +221,16 @@ bool rodeado(int posiciones[][2], int i)
     {
         return false;
     }
+}
+
+int suma_array(int array[], int n)
+{
+    int suma = 0;
+
+    for (int i = 0; i < n; i++)
+    {
+        suma += array[i];
+    }
+
+    return suma;
 }
